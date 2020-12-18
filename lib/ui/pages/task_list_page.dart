f5629a4d4ed7337611d05ffcc4d07737e7007db6
Lucia Montesinos <lucia.montesinos@live.u-tad.com>
2020-12-18 09:24:27 +0100

    Task with model

--- lib/ui/pages/task_list_page.dart
+++ lib/ui/pages/task_list_page.dart
@@ -8,23 +8,38 @@ class TaskList extends StatelessWidget {
         body: Container(
       height: height,
       child: Stack(children: <Widget>[
-        Container(
-            padding: EdgeInsets.symmetric(horizontal: 5),
-            child: SingleChildScrollView(
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                  SizedBox(height: height * .08),
-                  CarpAppBar(),
-                  ScoreBoard(),
-                  SizedBox(height: 15),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text('MY TASKS', style: sectionTitleStyle, textAlign: TextAlign.left),
-                  ),
-                  task('My mood yesterday', 1, DateTime.now()),
-                ])))
+        StreamBuilder<UserTask>(
+            stream: TaskListPageModel().userTaskEvents,
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) return CircularProgressIndicator();
+              return Container(
+                  padding: EdgeInsets.symmetric(horizontal: 5),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        SizedBox(height: height * .08),
+                        CarpAppBar(),
+                        scoreBoard(TaskListPageModel().daysInStudy, TaskListPageModel().taskCompleted),
+                        SizedBox(height: 15),
+                        Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 15),
+                            child: Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text('MY TASKS', style: sectionTitleStyle),
+                            )),
+                        SingleChildScrollView(
+                            child: ListView.builder(
+                                reverse: true,
+                                shrinkWrap: true,
+                                physics: NeverScrollableScrollPhysics(),
+                                // -1 because these are "Previous Chapters, so we don't want to show the latest one
+                                //itemCount: snapshot.data.chapters.length - 1,
+                                itemBuilder: (context, index) {
+                                  return task(snapshot.data.task.title, 1, DateTime.now());
+                                })),
+                      ]));
+            })
       ]
           //child: CircularProgressIndicator(),
           ),
@@ -40,6 +55,48 @@ class TaskList extends StatelessWidget {
         ),
         title: Text(title, style: aboutCardTitleStyle),
         subtitle: Text(timeToComplete.toString() + 'min to complete -' + date.day.toString()),
+      ),
+      margin: EdgeInsets.all(10),
+      elevation: 5,
+    );
+  }
+
+  Widget scoreBoard(int daysInStudy, int taskCompleted) {
+    return Container(
+      width: 400,
+      height: 110,
+      color: Color(0xFFF1F9FF),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(height: 15),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  Text(daysInStudy.toString(), style: scoreNumberStyle),
+                  Text('Days in study', style: scoreTextStyle),
+                ],
+              ),
+              Container(
+                  height: 66,
+                  child: VerticalDivider(
+                    color: Color(0xff77A8C8),
+                    width: 80,
+                  )),
+              Column(
+                children: [
+                  Text(taskCompleted.toString(), style: scoreNumberStyle),
+                  Text('Task completed', style: scoreTextStyle),
+                ],
+              )
+            ],
+          ),
+          SizedBox(height: 15),
+        ],
       ),
     );
   }

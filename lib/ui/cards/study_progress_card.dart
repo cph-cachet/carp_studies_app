3715910c1cb8973dd8035a300f6dc54e1f6b2cf5
Lucia Montesinos <lucia.montesinos@live.u-tad.com>
2021-07-01 13:25:09 +0200

    Update icon, No-task Screen

--- lib/ui/cards/study_progress_card.dart
+++ lib/ui/cards/study_progress_card.dart
@@ -7,29 +7,10 @@ class StudyProgressCardWidget extends StatefulWidget {
   StudyProgressCardWidget(this.model, {this.colors = const [CACHET.BLUE_1, CACHET.RED_1, CACHET.BLUE_3]});
 
   @override
-  _StudyProgressCardWidgetState createState() =>
-      _StudyProgressCardWidgetState();
+  _StudyProgressCardWidgetState createState() => _StudyProgressCardWidgetState();
 }
 
 class _StudyProgressCardWidgetState extends State<StudyProgressCardWidget> {
-  charts.RenderSpec<num> renderSpecNum = AxisTheme.axisThemeNum();
-  charts.RenderSpec<DateTime> renderSpecTime = AxisTheme.axisThemeDateTime();
-  charts.RenderSpec<String> renderSpecString = AxisTheme.axisThemeOrdinal();
-
-  // static List<charts.Series<StudyProgress, String>> _createChartList(
-  //         BuildContext context, StudyProgressCardDataModel model, List<Color> colors) =>
-  //     [
-  //       charts.Series<StudyProgress, String>(
-  //           colorFn: (_, index) => charts.ColorUtil.fromDartColor(colors[index]),
-  //           //charts.MaterialPalette.blue.makeShades(min(7, model.samplingTable.length))[index],
-  //           id: 'progress',
-  //           data: model.progress,
-  //           domainFn: (StudyProgress datum, _) => datum.state,
-  //           measureFn: (StudyProgress datum, _) => datum.value,
-  //           labelAccessorFn: (StudyProgress datum, _) => '${datum.value}'),
-
-  //     ];
-
   @override
   Widget build(BuildContext context) {
     AssetLocalizations locale = AssetLocalizations.of(context);
@@ -60,10 +41,7 @@ class _StudyProgressCardWidgetState extends State<StudyProgressCardWidget> {
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: <Widget>[
                                   SizedBox(height: 5),
-                                  Text(
-                                      locale.translate(
-                                          'cards.study_progress.title'),
-                                      //textAlign: TextAlign.center,
+                                  Text(locale.translate('cards.study_progress.title'),
                                       style: dataCardTitleStyle),
                                 ],
                               ),
@@ -73,21 +51,6 @@ class _StudyProgressCardWidgetState extends State<StudyProgressCardWidget> {
                       ),
                       Container(
                           height: 160,
-                          // child: charts.BarChart(
-                          //   _createChartList(context, widget.model, CACHET.COLOR_LIST),
-                          //   barGroupingType: charts.BarGroupingType.groupedStacked,
-                          //   animate: true,
-                          //   vertical: false,
-                          //   barRendererDecorator: charts.BarLabelDecorator<String>(),
-                          //   //domainAxis: charts.OrdinalAxisSpec(renderSpec: renderSpecString),
-                          //   //primaryMeasureAxis: charts.NumericAxisSpec(renderSpec: renderSpecNum),
-                          //   //userManagedState: _myState,
-                          //   defaultInteractions: true,
-                          //   behaviors: [
-                          //     charts.SelectNearest(eventTrigger: charts.SelectionTrigger.tapAndDrag),
-                          //     charts.DomainHighlighter(),
-                          //   ],
-                          // ),
                           child: HorizontalBar(
                               names: this.widget.model.progress.map((progress) => progress.state).toList(),
                               values: this.widget.model.progress.map((progress) => progress.value).toList(),

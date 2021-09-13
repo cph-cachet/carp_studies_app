45444c63719c8815351c869fc7c38f80def2f5c1
Lucia Montesinos <lucia.montesinos@live.u-tad.com>
2021-09-13 14:56:01 +0200

    Carp banner

--- lib/ui/widgets/carp_app_bar.dart
+++ lib/ui/widgets/carp_app_bar.dart
@@ -7,27 +7,25 @@ class CarpAppBar extends StatelessWidget {
       children: [
         SizedBox(height: 35),
         Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Container(
+              margin: const EdgeInsets.only(left: 15.0),
               child: Image.asset(
-                'assets/cachet_logo.png',
-                height: 20,
+                'assets/carp_logo.png',
+                height: 30,
               ),
             ),
             SizedBox(width: 3),
             IconButton(
-              icon: Icon(Icons.account_circle_outlined,
-                  color: Theme.of(context).primaryColor, size: 30),
+              icon: Icon(Icons.account_circle_outlined, color: Theme.of(context).primaryColor, size: 30),
               tooltip: 'Profile',
               onPressed: () {
                 Navigator.push(
                   context,
                   PageRouteBuilder(
-                    pageBuilder: (c, a1, a2) =>
-                        ProfilePage(bloc.data.profilePageModel),
-                    transitionsBuilder: (c, anim, a2, child) =>
-                        FadeTransition(opacity: anim, child: child),
+                    pageBuilder: (c, a1, a2) => ProfilePage(bloc.data.profilePageModel),
+                    transitionsBuilder: (c, anim, a2, child) => FadeTransition(opacity: anim, child: child),
                     transitionDuration: Duration(milliseconds: 200),
                   ),
                 );

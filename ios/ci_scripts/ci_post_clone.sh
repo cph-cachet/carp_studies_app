1fdcf967699d55c6350fc8b361ba027b1dda7459
Lars Refsgaard <1552887+LarsRefsgaard@users.noreply.github.com>
2022-12-09 16:29:27 +0100

    try installing flutter with brew instead of git

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -4,8 +4,8 @@
 cd $CI_WORKSPACE # change working directory to the root of your cloned repo.
 
 # Install Flutter using git.
-git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
-export PATH="$PATH:$HOME/flutter/bin"
+# git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
+# export PATH="$PATH:$HOME/flutter/bin"
 
 # Install Flutter artifacts for iOS (--ios), or macOS (--macos) platforms.
 flutter precache --ios
@@ -16,6 +16,8 @@ flutter pub get
 # Install CocoaPods using Homebrew.
 HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates.
 brew install cocoapods
+
+brew install flutter
 
 # Install CocoaPods dependencies.
 cd ios && pod install # run `pod install` in the `ios` directory.

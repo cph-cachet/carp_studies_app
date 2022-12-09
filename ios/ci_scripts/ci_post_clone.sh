786a8afe211027b1df8d9f997922aa59e9c36857
Lars Refsgaard <1552887+LarsRefsgaard@users.noreply.github.com>
2022-12-09 16:03:40 +0100

    Incorporate Xcode Cloud to have every push create a new testflight build Fixes #123

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -17,8 +17,6 @@ flutter pub get
 HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates.
 brew install cocoapods
 
-curl https://raw.githubusercontent.com/cph-cachet/flutter-plugins/master/packages/esense_flutter/ios/esense_flutter.podspec > ios/.symlinks/plugins/esense_flutter/ios/esense_flutter.podspec
-
 # Install CocoaPods dependencies.
 cd ios && pod install # run `pod install` in the `ios` directory.
 

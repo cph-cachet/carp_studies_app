2f157bf8c6ab5c4c2988d3cd501868b670affce1
Lars Refsgaard <1552887+LarsRefsgaard@users.noreply.github.com>
2022-12-09 17:04:59 +0100

    revert to flutter install over git

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -3,11 +3,9 @@
 # The default execution directory of this script is the ci_scripts directory.
 cd $CI_WORKSPACE # change working directory to the root of your cloned repo.
 
-HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates.
 # Install Flutter using git.
-# git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
-# export PATH="$PATH:$HOME/flutter/bin"
-brew install flutter
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
+export PATH="$PATH:$HOME/flutter/bin"
 
 # Install Flutter artifacts for iOS (--ios), or macOS (--macos) platforms.
 flutter precache --ios
@@ -16,8 +14,8 @@ flutter precache --ios
 flutter pub get
 
 # Install CocoaPods using Homebrew.
+HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates.
 brew install cocoapods
-
 
 # Install CocoaPods dependencies.
 cd ios && pod install # run `pod install` in the `ios` directory.

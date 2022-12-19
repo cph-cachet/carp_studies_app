d74cb7ef7f9ebdbf8e34b1624fa5a09d7cb6633f
Lars Refsgaard <1552887+LarsRefsgaard@users.noreply.github.com>
2022-12-19 13:46:34 +0100

    dont build to distribute

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -25,23 +25,3 @@ import flutter_local_notifications
   }
 }
 
-
-/*
-
- OLD VERSION BELOW
-
-import UIKit
-import Flutter
-
-@UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-}
-
-*/

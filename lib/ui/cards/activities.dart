77eabd225c5f815e353e297c9a3e6198f4e1b0fa
Lars Refsgaard <1552887+LarsRefsgaard@users.noreply.github.com>
2023-05-22 11:44:00 +0200

    Nice looking activity diagram

--- lib/ui/cards/activities.dart
+++ lib/ui/cards/activities.dart
@@ -17,7 +17,9 @@ class ActivitiesWidgetState extends State<ActivitiesWidget> {
   num? _run = 0;
   num? _cycle = 0;
 
-  final betweenSpace = 0.2;
+  num maxValue = 0;
+
+  final betweenSpace = 2.4;
 
   @override
   void initState() {
@@ -59,6 +61,7 @@ class ActivitiesWidgetState extends State<ActivitiesWidget> {
             ),
             SizedBox(
               height: 240,
+              width: MediaQuery.of(context).size.width * 0.9,
               child: barCharts,
             ),
           ],
@@ -72,27 +75,154 @@ class ActivitiesWidgetState extends State<ActivitiesWidget> {
 
     return BarChart(
       BarChartData(
-          alignment: BarChartAlignment.spaceBetween,
-          titlesData: FlTitlesData(
-            bottomTitles: AxisTitles(
-                sideTitles: SideTitles(
-                    showTitles: true,
-                    getTitlesWidget: bottomTitles,
-                    reservedSize: 20)),
-            leftTitles: AxisTitles(),
-            rightTitles: AxisTitles(
-              sideTitles: SideTitles(
-                showTitles: false,
-                getTitlesWidget: rightTitles,
-                reservedSize: 20,
-              ),
+        alignment: BarChartAlignment.spaceBetween,
+        titlesData: FlTitlesData(
+          bottomTitles: AxisTitles(
+            sideTitles: SideTitles(
+              showTitles: true,
+              getTitlesWidget: bottomTitles,
+              reservedSize: 20,
             ),
-            topTitles: AxisTitles(),
           ),
-          barTouchData: BarTouchData(
-            enabled: false,
+          leftTitles: AxisTitles(),
+          rightTitles: AxisTitles(
+            sideTitles: SideTitles(
+              showTitles: true,
+              getTitlesWidget: rightTitles,
+              reservedSize: 48,
+            ),
           ),
-          barGroups: convertMapToList(widget.model.activities)),
+          topTitles: AxisTitles(),
+        ),
+        barTouchData: BarTouchData(
+          enabled: false,
+        ),
+        barGroups: convertMapToList(widget.model.activities),
+        maxY: (maxValue) * 1.2,
+        gridData: FlGridData(
+          show: true,
+          drawVerticalLine: false,
+          drawHorizontalLine: true,
+          getDrawingHorizontalLine: (value) {
+            return FlLine(
+              color: Colors.grey.withOpacity(0.3),
+              strokeWidth: 1,
+            );
+          },
+        ),
+        borderData: FlBorderData(
+          show: false,
+          border: Border.all(
+            width: 1,
+            color: Colors.grey.withOpacity(0.2),
+          ),
+        ),
+      ),
+    );
+  }
+
+  /// Doing some conversions to make the data readable by the chart
+  /// The data is organized in a list of lists, where each list represents a day
+  /// and each element in the list represents the time spent doing a specific
+  /// activity.
+  List<BarChartGroupData> convertMapToList(
+      Map<ActivityType, Map<int, int>> data) {
+    List<List<int>> resultList = List.generate(
+        7, (_) => List.generate(4, (index) => index, growable: false),
+        growable: false);
+
+    data.forEach((activityType, innerMap) {
+        int idx;
+        if (activityType == ActivityType.WALKING) {
+          idx = 1;
+        } else if (activityType == ActivityType.RUNNING) {
+          idx = 2;
+        } else if (activityType == ActivityType.ON_BICYCLE) {
+          idx = 3;
+        } else {
+          return;
+        }
+      innerMap.forEach((weekday, time) {
+        resultList[weekday - 1][0] = weekday; // Assign weekday at index 0
+        resultList[weekday - 1][idx] = time;
+
+      });
+    });
+
+    List<BarChartGroupData> result = resultList
+        .map((e) => generateGroupData(e[0], e[1], e[2], e[3]))
+        .toList();
+
+    return result;
+  }
+
+  BarChartGroupData generateGroupData(
+    int x,
+    num walking,
+    num running,
+    num cycling,
+  ) {
+    double roundness = 2;
+    maxValue = max(maxValue, walking + running + cycling);
+
+    return BarChartGroupData(
+      x: x,
+      groupVertically: true,
+      barRods: [
+        BarChartRodData(
+            fromY: 0,
+            toY: walking + 0,
+            color: widget.colors[0],
+            width: 32,
+            borderRadius: BorderRadius.all(Radius.circular(roundness))),
+        BarChartRodData(
+            fromY: walking + betweenSpace,
+            toY: walking + betweenSpace + running,
+            color: widget.colors[1],
+            width: 32,
+            borderRadius: BorderRadius.all(Radius.circular(roundness))),
+        BarChartRodData(
+          fromY: walking + betweenSpace + running + betweenSpace,
+          toY: walking + betweenSpace + running + betweenSpace + cycling,
+          color: widget.colors[2],
+          width: 32,
+          borderRadius: BorderRadius.only(
+            topLeft: const Radius.circular(8),
+            topRight: const Radius.circular(8),
+            bottomLeft: Radius.circular(roundness),
+            bottomRight: Radius.circular(roundness),
+          ),
+        ),
+      ],
+    );
+  }
+
+  TextStyle activityVisualisationTextStyle(
+      {double? fontSize, Color? color, List<ui.FontFeature>? fontFeatures}) {
+    return GoogleFonts.barlow(
+      fontSize: fontSize,
+      fontWeight: FontWeight.w600,
+      color: color,
+      fontFeatures: fontFeatures,
+    );
+  }
+
+  Widget rightTitles(double value, TitleMeta meta) {
+    final text = value.toInt() % meta.appliedInterval == 0
+        ? value.toInt().toString()
+        : '';
+
+    final style = activityVisualisationTextStyle(
+      color: Colors.grey.withOpacity(0.6),
+      fontSize: 14,
+    );
+    return SideTitleWidget(
+      axisSide: AxisSide.right,
+      space: 16,
+      child: Text(
+        text,
+        style: style,
+      ),
     );
   }
 
@@ -127,102 +257,6 @@ class ActivitiesWidgetState extends State<ActivitiesWidget> {
     return SideTitleWidget(
       axisSide: meta.axisSide,
       child: Text(text, style: style),
-    );
-  }
-
-  /// Doing some conversions to make the data readable by the chart
-  /// The data is organized in a list of lists, where each list represents a day
-  /// and each element in the list represents the time spent doing a specific
-  /// activity.
-  List<BarChartGroupData> convertMapToList(
-      Map<ActivityType, Map<int, int>> data) {
-    List<List<int>> resultList = List.generate(
-        7, (_) => List.generate(4, (index) => index, growable: false),
-        growable: false);
-
-    data.forEach((activityType, innerMap) {
-      innerMap.forEach((weekday, time) {
-        int idx;
-        if (activityType == ActivityType.WALKING) {
-          idx = 0;
-        } else if (activityType == ActivityType.RUNNING) {
-          idx = 1;
-        } else if (activityType == ActivityType.ON_BICYCLE) {
-          idx = 2;
-        } else {
-          idx = -1;
-        }
-        resultList[weekday - 1][idx + 1] = time;
-      });
-    });
-
-    List<BarChartGroupData> result = resultList
-        .map((e) => generateGroupData(e[0], e[1], e[2], e[3]))
-        .toList();
-
-    return result;
-  }
-
-  BarChartGroupData generateGroupData(
-    int x,
-    num walking,
-    num running,
-    num cycling,
-  ) {
-    return BarChartGroupData(
-      x: x,
-      groupVertically: true,
-      barRods: [
-        BarChartRodData(
-          fromY: 0,
-          toY: walking + 0,
-          color: widget.colors[0],
-          width: 20,
-          borderRadius: const BorderRadius.only()
-        ),
-        BarChartRodData(
-          fromY: walking + betweenSpace,
-          toY: walking + betweenSpace + running,
-          color: widget.colors[1],
-          width: 20,
-          borderRadius: const BorderRadius.only()
-        ),
-        BarChartRodData(
-          fromY: walking + betweenSpace + running + betweenSpace,
-          toY: walking + betweenSpace + running + betweenSpace + cycling,
-          color: widget.colors[2],
-          width: 20,
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(10),
-            topRight: Radius.circular(10),
-          ),
-        ),
-      ],
-    );
-  }
-
-  TextStyle activityVisualisationTextStyle(
-      {double? fontSize, Color? color, List<ui.FontFeature>? fontFeatures}) {
-    return GoogleFonts.barlow(
-      fontSize: fontSize,
-      fontWeight: FontWeight.w600,
-      color: color,
-      fontFeatures: fontFeatures,
-    );
-  }
-
-  Widget rightTitles(double value, TitleMeta meta) {
-    final text = value.toInt().toString();
-    final style = activityVisualisationTextStyle(
-      color: Colors.grey.withOpacity(0.6),
-      fontSize: 14,
-    );
-    return SideTitleWidget(
-      axisSide: AxisSide.right,
-      child: Text(
-        text,
-        style: style,
-      ),
     );
   }
 }

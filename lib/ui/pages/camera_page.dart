4502905aa2e3721699fec97dbd845d20e7c1406b
Lars Refsgaard <1552887+LarsRefsgaard@users.noreply.github.com>
2023-06-08 11:12:55 +0200

    Wrap camera page in a `SafeArea`

--- lib/ui/pages/camera_page.dart
+++ lib/ui/pages/camera_page.dart
@@ -48,161 +48,163 @@ class CameraPageState extends State<CameraPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Column(
-        children: [
-          const SizedBox(height: 35),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              IconButton(
-                  onPressed: () {
-                    _showCancelConfirmationDialog();
-                  },
-                  icon: const Icon(Icons.close, color: Colors.white, size: 30))
-            ],
-          ),
-          const SizedBox(height: 35),
-          FutureBuilder<void>(
-            future: initializeCamera(selectedCamera),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: ClipRRect(
-                      borderRadius: const BorderRadius.all(Radius.circular(20)),
-                      child: SizedBox(
-                          height: MediaQuery.of(context).size.height * 0.6,
-                          width: MediaQuery.of(context).size.width * 0.9,
-                          child: CameraPreview(_cameraController!))),
-                );
-              } else {
-                return const Center(child: CircularProgressIndicator());
-              }
-            },
-          ),
-          const Spacer(),
-          Padding(
-            padding: const EdgeInsets.only(right: 10, left: 10, bottom: 30),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+      body: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(height: 35),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 IconButton(
-                  onPressed: () {
-                    if (cameras!.length > 1) {
-                      setState(() {
-                        selectedCamera = selectedCamera == 0 ? 1 : 0;
-                        initializeCamera(selectedCamera);
-                      });
-                    }
-                  },
-                  icon: const Icon(Icons.flip_camera_android,
-                      color: Colors.white),
-                ),
-                GestureDetector(
-                  onTap: () async {
-                    await initializeCamera(selectedCamera);
-                    var picture = await _cameraController!.takePicture();
-                    widget.videoUserTask.onPictureCapture(picture);
-                    if (context.mounted) {
-                      await Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => DisplayPicturePage(
-                                file: picture,
-                                videoUserTask: widget.videoUserTask,
-                              )));
-                    }
-
-                    setState(() {
-                      capturedImages = File(picture.path);
-                    });
-                  },
-                  onLongPress: () async {
-                    await initializeCamera(selectedCamera);
-
-                    try {
-                      await _cameraController!.startVideoRecording();
-                      widget.videoUserTask.onRecordStart();
-                      setState(() {
-                        isRecording = true;
-                      });
-                    } on CameraException catch (e) {
-                      warning(
-                          '$runtimeType - error: ${e.code}\n${e.description}');
-                    }
-                  },
-                  onLongPressEnd: (details) async {
-                    try {
-                      var video = await _cameraController!.stopVideoRecording();
-
-                      widget.videoUserTask.onRecordStop(video);
-                      if (context.mounted) {
-                        await Navigator.of(context).push(
-                          MaterialPageRoute(
-                              builder: (context) => DisplayPicturePage(
-                                  file: video,
-                                  isVideo: true,
-                                  videoUserTask: widget.videoUserTask)),
-                        );
-                      }
-                      setState(() {
-                        capturedImages = File(video.path);
-                        isRecording = false;
-                      });
-                    } on CameraException catch (e) {
-                      warning(
-                          '$runtimeType - error: ${e.code}\n${e.description}');
-                    }
-                  },
-                  child: isRecording
-                      ? Stack(
-                          alignment: Alignment.center,
-                          children: [
-                            const SizedBox(
-                              width: 65,
-                              height: 65,
-                              child: CircularProgressIndicator(
-                                  backgroundColor: Colors.white54,
-                                  valueColor:
-                                      AlwaysStoppedAnimation(Colors.black54),
-                                  strokeWidth: 5),
-                            ),
-                            Container(
-                              height: 60,
-                              width: 60,
-                              decoration: const BoxDecoration(
-                                  shape: BoxShape.circle, color: Colors.white),
-                            ),
-                          ],
-                        )
-                      : Container(
-                          height: 60,
-                          width: 60,
-                          decoration: const BoxDecoration(
-                              shape: BoxShape.circle, color: Colors.white),
-                        ),
-                ),
-                IconButton(
-                  onPressed: () {
-                    if (isFlashOff) {
-                      setState(() {
-                        isFlashOff = false;
-                        flashIcon = Icons.flash_on;
-                      });
-                      _cameraController?.setFlashMode(FlashMode.always);
-                    } else {
-                      setState(() {
-                        isFlashOff = true;
-                        flashIcon = Icons.flash_off;
-                      });
-                      _cameraController?.setFlashMode(FlashMode.off);
-                    }
-                  },
-                  icon: Icon(flashIcon, color: Colors.white),
-                ),
+                    onPressed: () {
+                      _showCancelConfirmationDialog();
+                    },
+                    icon: const Icon(Icons.close, color: Colors.white, size: 30))
               ],
             ),
-          ),
-          const Spacer(),
-        ],
+            const SizedBox(height: 35),
+            FutureBuilder<void>(
+              future: initializeCamera(selectedCamera),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: ClipRRect(
+                        borderRadius: const BorderRadius.all(Radius.circular(20)),
+                        child: SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.6,
+                            width: MediaQuery.of(context).size.width * 0.9,
+                            child: CameraPreview(_cameraController!))),
+                  );
+                } else {
+                  return const Center(child: CircularProgressIndicator());
+                }
+              },
+            ),
+            const Spacer(),
+            Padding(
+              padding: const EdgeInsets.only(right: 10, left: 10, bottom: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      if (cameras!.length > 1) {
+                        setState(() {
+                          selectedCamera = selectedCamera == 0 ? 1 : 0;
+                          initializeCamera(selectedCamera);
+                        });
+                      }
+                    },
+                    icon: const Icon(Icons.flip_camera_android,
+                        color: Colors.white),
+                  ),
+                  GestureDetector(
+                    onTap: () async {
+                      await initializeCamera(selectedCamera);
+                      var picture = await _cameraController!.takePicture();
+                      widget.videoUserTask.onPictureCapture(picture);
+                      if (context.mounted) {
+                        await Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => DisplayPicturePage(
+                                  file: picture,
+                                  videoUserTask: widget.videoUserTask,
+                                )));
+                      }
+      
+                      setState(() {
+                        capturedImages = File(picture.path);
+                      });
+                    },
+                    onLongPress: () async {
+                      await initializeCamera(selectedCamera);
+      
+                      try {
+                        await _cameraController!.startVideoRecording();
+                        widget.videoUserTask.onRecordStart();
+                        setState(() {
+                          isRecording = true;
+                        });
+                      } on CameraException catch (e) {
+                        warning(
+                            '$runtimeType - error: ${e.code}\n${e.description}');
+                      }
+                    },
+                    onLongPressEnd: (details) async {
+                      try {
+                        var video = await _cameraController!.stopVideoRecording();
+      
+                        widget.videoUserTask.onRecordStop(video);
+                        if (context.mounted) {
+                          await Navigator.of(context).push(
+                            MaterialPageRoute(
+                                builder: (context) => DisplayPicturePage(
+                                    file: video,
+                                    isVideo: true,
+                                    videoUserTask: widget.videoUserTask)),
+                          );
+                        }
+                        setState(() {
+                          capturedImages = File(video.path);
+                          isRecording = false;
+                        });
+                      } on CameraException catch (e) {
+                        warning(
+                            '$runtimeType - error: ${e.code}\n${e.description}');
+                      }
+                    },
+                    child: isRecording
+                        ? Stack(
+                            alignment: Alignment.center,
+                            children: [
+                              const SizedBox(
+                                width: 65,
+                                height: 65,
+                                child: CircularProgressIndicator(
+                                    backgroundColor: Colors.white54,
+                                    valueColor:
+                                        AlwaysStoppedAnimation(Colors.black54),
+                                    strokeWidth: 5),
+                              ),
+                              Container(
+                                height: 60,
+                                width: 60,
+                                decoration: const BoxDecoration(
+                                    shape: BoxShape.circle, color: Colors.white),
+                              ),
+                            ],
+                          )
+                        : Container(
+                            height: 60,
+                            width: 60,
+                            decoration: const BoxDecoration(
+                                shape: BoxShape.circle, color: Colors.white),
+                          ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      if (isFlashOff) {
+                        setState(() {
+                          isFlashOff = false;
+                          flashIcon = Icons.flash_on;
+                        });
+                        _cameraController?.setFlashMode(FlashMode.always);
+                      } else {
+                        setState(() {
+                          isFlashOff = true;
+                          flashIcon = Icons.flash_off;
+                        });
+                        _cameraController?.setFlashMode(FlashMode.off);
+                      }
+                    },
+                    icon: Icon(flashIcon, color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+            const Spacer(),
+          ],
+        ),
       ),
     );
   }

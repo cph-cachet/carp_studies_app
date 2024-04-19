53b9a64f90cc51c33e5b4643fdc62e4852eb5559
Panosfunk <panosfunk@yahoo.gr>
2024-04-19 17:13:01 +0200

    testing to see if i get this message in the 'Test Details' note in TestFlight.

--- ios/ci_scripts/ci_post_xcodebuild.sh
+++ ios/ci_scripts/ci_post_xcodebuild.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/zsh
 
 
 if [[ -d "$CI_APP_STORE_SIGNED_APP_PATH" ]]; then
   TESTFLIGHT_DIR_PATH=../TestFlight
   mkdir $TESTFLIGHT_DIR_PATH
-  git fetch --deepen 3 && git log -1 --pretty=format:"%s" >! $TESTFLIGHT_DIR_PATH/CommitNotes.en-US.txt
+  git fetch --deepen 1 && git log -1 --pretty=format:"%s" >! $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
 fi

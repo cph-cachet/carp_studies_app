9cb0fa3023c8b6b75efc3d6bab4447e31d7fb945
Aamir Farooq <SlimShadyIAm@users.noreply.github.com>
2024-04-23 13:52:19 +0200

    Improve subtitle for tasks in task list to be more informative of the actual time (#266)
    * Add util for humanizing time remaining and use it in task list
    
    * Fix formatting
    
    * Change parameter passed into util

--- lib/blocs/util.dart
+++ lib/blocs/util.dart
@@ -4,3 +4,28 @@ extension StringExtension on String {
   String truncateTo(int maxLength) =>
       (length <= maxLength) ? this : '${substring(0, maxLength)}...';
 }
+
+extension Humanize on Duration {
+  String humanize(RPLocalizations locale) {
+    // Convert the difference into a human-readable format
+    if (inSeconds < 60) {
+      return _pluralize(inSeconds, locale, 'seconds');
+    } else if (inMinutes < 60) {
+      return _pluralize(inMinutes, locale, 'minutes');
+    } else if (inHours < 24) {
+      return _pluralize(inHours, locale, 'hours');
+    } else {
+      return _pluralize(inDays, locale, 'days');
+    }
+  }
+
+  String _pluralize(int n, RPLocalizations locale, String unit) {
+    String translationKey = "pages.task_list.task.${unit}_remaining";
+
+    if (n != 1) {
+      translationKey += "_plural";
+    }
+
+    return "$n ${locale.translate(translationKey)}";
+  }
+}

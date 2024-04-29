9f386a8efdf6f6c502bd56871927e211ed23a5a1
Panosfunk <panosfunk@yahoo.gr>
2024-04-29 18:30:24 +0200

    added CarpAppBar to InvitationPage.

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'package:async/async.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'package:json_annotation/json_annotation.dart';
--- lib/ui/pages/invitation_page.dart
+++ lib/ui/pages/invitation_page.dart
@@ -16,87 +16,234 @@ class InvitationDetailsPage extends StatelessWidget {
     RPLocalizations locale = RPLocalizations.of(context)!;
     var invitation = model.getInvitation(invitationId);
 
+    // return Scaffold(
+    //   body: Padding(
+    //     padding: const EdgeInsets.only(bottom: 16.0, left: 16.0, right: 16.0),
+    //     child: CustomScrollView(
+    //       slivers: [
+    //         SliverAppBar(
+    //           title: const CarpAppBar(),
+    //           automaticallyImplyLeading: false,
+    //           centerTitle: true,
+    //           pinned: false,
+    //           stretch: true,
+    //           stretchTriggerOffset: 20,
+    //         ),
+    //         SliverToBoxAdapter(
+    //           child: IntrinsicHeight(
+    //             child: Stack(
+    //               children: [
+    //                 Positioned(
+    //                   left: 0,
+    //                   child: IconButton(
+    //                     icon: const Icon(Icons.arrow_back_ios),
+    //                     onPressed: () {
+    //                       if (context.canPop()) {
+    //                         context.pop();
+    //                       } else {
+    //                         context.go(LoginPage.route);
+    //                       }
+    //                     },
+    //                   ),
+    //                 ),
+    //                 Container(
+    //                   padding: EdgeInsets.only(bottom: 10),
+    //                   child: Align(
+    //                     child: Center(
+    //                       child: Text(
+    //                         locale.translate('invitation.invited_to_study'),
+    //                         textAlign: TextAlign.center,
+    //                         style: const TextStyle(
+    //                           fontWeight: FontWeight.bold,
+    //                           fontSize: 22.0,
+    //                         ),
+    //                       ),
+    //                     ),
+    //                   ),
+    //                 ),
+    //               ],
+    //             ),
+    //           ),
+    //         ),
+    //         SliverToBoxAdapter(
+    //           child: Container(
+    //             child: Material(
+    //               elevation: 8.0,
+    //               shape: RoundedRectangleBorder(
+    //                 borderRadius: BorderRadius.circular(
+    //                     12.0), // Adjust the radius as needed
+    //               ),
+    //               child: Padding(
+    //                 padding: const EdgeInsets.all(16.0),
+    //                 child: Column(
+    //                   crossAxisAlignment: CrossAxisAlignment.stretch,
+    //                   children: [
+    //                     Text(
+    //                       invitation.invitation.name,
+    //                       style: const TextStyle(
+    //                         fontWeight: FontWeight.bold,
+    //                         fontSize: 22.0,
+    //                       ),
+    //                     ),
+    //                     Scrollbar(
+    //                       thumbVisibility: true,
+    //                       radius: const Radius.circular(100),
+    //                       child: SingleChildScrollView(
+    //                         padding: const EdgeInsets.all(16.0),
+    //                         child: Text(
+    //                           invitation.invitation.description ?? '',
+    //                           style: const TextStyle(fontSize: 16.0),
+    //                         ),
+    //                       ),
+    //                     ),
+    //                   ],
+    //                 ),
+    //               ),
+    //             ),
+    //           ),
+    //         ),
+    //         SliverToBoxAdapter(
+    //           child: Container(
+    //             margin: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
+    //             height: 56,
+    //             decoration: BoxDecoration(
+    //               color: const Color(0xff006398),
+    //               borderRadius: BorderRadius.circular(100),
+    //             ),
+    //             child: TextButton(
+    //               onPressed: () {
+    //                 bloc.setStudyInvitation(invitation, context);
+    //                 context.push(InformedConsentPage.route);
+    //               },
+    //               child: Text(
+    //                 locale.translate("invitation.accept_invite"),
+    //                 style:
+    //                     const TextStyle(color: Color(0xffffffff), fontSize: 22),
+    //                 textAlign: TextAlign.center,
+    //               ),
+    //             ),
+    //           ),
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    // );
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          locale.translate('invitation.invited_to_study'),
-          textAlign: TextAlign.center,
-          style: const TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 22.0,
-          ),
+        title: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: const CarpAppBar(),
         ),
+        automaticallyImplyLeading: false,
         centerTitle: true,
         elevation: 0.0,
       ),
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Material(
-                  elevation: 8.0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(
-                        12.0), // Adjust the radius as needed
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                      right: 24.0,
-                      left: 24.0,
-                      top: 16.0,
-                      bottom: 16.0, // Adjust the bottom padding
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              IntrinsicHeight(
+                child: Stack(
+                  children: [
+                    Positioned(
+                      left: 0,
+                      child: IconButton(
+                        icon: const Icon(Icons.arrow_back_ios),
+                        onPressed: () {
+                          if (context.canPop()) {
+                            context.pop();
+                          } else {
+                            context.go(LoginPage.route);
+                          }
+                        },
+                      ),
                     ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Text(
-                          invitation.invitation.name,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 22.0,
-                          ),
-                        ),
-                        Scrollbar(
-                          thumbVisibility: true,
-                          radius: const Radius.circular(100),
-                          child: SingleChildScrollView(
-                            padding: const EdgeInsets.all(16.0),
-                            child: Text(
-                              invitation.invitation.description ?? '',
-                              style: const TextStyle(fontSize: 16.0),
+                    Container(
+                      padding: EdgeInsets.only(bottom: 10),
+                      child: Align(
+                        child: Center(
+                          child: Text(
+                            locale.translate('invitation.invited_to_study'),
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 22.0,
                             ),
                           ),
                         ),
-                      ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: Material(
+                    elevation: 8.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                          12.0), // Adjust the radius as needed
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        right: 24.0,
+                        left: 24.0,
+                        top: 16.0,
+                        bottom: 16.0, // Adjust the bottom padding
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Text(
+                            invitation.invitation.name,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 22.0,
+                            ),
+                          ),
+                          Scrollbar(
+                            thumbVisibility: true,
+                            radius: const Radius.circular(100),
+                            child: SingleChildScrollView(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Text(
+                                invitation.invitation.description ?? '',
+                                style: const TextStyle(fontSize: 16.0),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
-              height: 56,
-              decoration: BoxDecoration(
-                color: const Color(0xff006398),
-                borderRadius: BorderRadius.circular(100),
-              ),
-              child: TextButton(
-                onPressed: () {
-                  bloc.setStudyInvitation(invitation, context);
-                  context.push(InformedConsentPage.route);
-                },
-                child: Text(
-                  locale.translate("invitation.accept_invite"),
-                  style:
-                      const TextStyle(color: Color(0xffffffff), fontSize: 22),
-                  textAlign: TextAlign.center,
+              Container(
+                margin: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
+                height: 56,
+                decoration: BoxDecoration(
+                  color: const Color(0xff006398),
+                  borderRadius: BorderRadius.circular(100),
+                ),
+                child: TextButton(
+                  onPressed: () {
+                    bloc.setStudyInvitation(invitation, context);
+                    context.push(InformedConsentPage.route);
+                  },
+                  child: Text(
+                    locale.translate("invitation.accept_invite"),
+                    style:
+                        const TextStyle(color: Color(0xffffffff), fontSize: 22),
+                    textAlign: TextAlign.center,
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );

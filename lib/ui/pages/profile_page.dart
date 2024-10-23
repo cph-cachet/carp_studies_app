d4798ab47afa164fdd50d8b1d78ce2365a27ff7b
Panagiotis Giannoutsos <36935711+Panosfunk@users.noreply.github.com>
2024-10-23 16:20:29 +0200

    formatting

--- lib/ui/pages/profile_page.dart
+++ lib/ui/pages/profile_page.dart
@@ -302,7 +302,8 @@ class SlidePageRoute extends PageRouteBuilder<Widget> {
             var begin = Offset(1.0, 0.0);
             var end = Offset.zero;
             var curve = Curves.easeInOut;
-            var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+            var tween =
+                Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
             var offsetAnimation = animation.drive(tween);
             return SlideTransition(
               position: offsetAnimation,

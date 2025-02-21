ce97aa18482a67dc27dd4bc5a1d8130a79d08838
Panagiotis Giannoutsos <36935711+Panosfunk@users.noreply.github.com>
2025-02-21 17:02:53 +0100

    post clone update

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -6,16 +6,20 @@ cd $CI_PRIMARY_REPOSITORY_PATH
 # Install Flutter using git.
 curl -sL https://github.com/flutter-actions/setup-flutter/releases/download/v4.0.0/setup-flutter.sh | bash
 export PATH="$PATH:$HOME/flutter/bin"
+echo "Flutter version: $(flutter --version)"
 
 # Install Flutter artifacts for iOS (--ios), or macOS (--macos) platforms.
 flutter precache --ios
 
 # Install Flutter dependencies.
+echo "Installing Flutter dependencies"
 flutter pub get
 
 # Install CocoaPods using Homebrew.
+echo "Installing CocoaPods"
 HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates.
 brew install cocoapods
+echo "CocoaPods version: $(pod --version)"
 
 # Install CocoaPods dependencies.
 cd ios && pod install

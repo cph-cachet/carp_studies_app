50dfe935328a4ce63f1b767ffe3dd0e1b4dd78f6
Panagiotis Giannoutsos <36935711+Panosfunk@users.noreply.github.com>
2025-08-22 16:27:48 +0200

    update post clone script

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -22,6 +22,7 @@ brew install cocoapods
 # Install CocoaPods dependencies.
 cd ios && pod install
 
-# flutter build ios --config-only --release
+flutter build ios --config-only --release \
+  --dart-define=deployment-mode=test
 
 exit 0
